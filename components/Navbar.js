40728b4e1125985bddb37ee62c2c9d553ae75ec6
Harrison Given <hgiven@apple.com>
2023-05-01 17:35:00 -0700

    remvoed ./Logo

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,7 +1,5 @@
 import Link from "next/link";
-import Image from "next/image";
 import React, { useState } from "react";
-import Logo from "./Logo";
 import NavItem from "./NavItem";
 
 const MENU_LIST = [
