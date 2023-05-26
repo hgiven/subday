f53c8c5e5302e69d89023ff4806d6256d5927010
Harrison Given <harrisonjbg@gmail.com>
2023-05-25 19:37:23 -0700

    added Navbar to _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
+import Navbar from '../components/Navbar'
 import '../styles/global.css';
 
 export default function App({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+    return (
+    <>
+        <Navbar />
+        <Component {...pageProps} />
+    </>
+    )
 }
