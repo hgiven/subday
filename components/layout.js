eafb432ce4785a8fb6b62229d1d91d604c2d622e
Harrison Given <harrisonjbg@gmail.com>
2023-05-25 19:40:01 -0700

    removed <header> from layout.js

--- components/layout.js
+++ components/layout.js
@@ -12,40 +12,6 @@ export default function Layout({ children, home }) {
     return (
       
       <div className={styles.container}>
-        <header className={styles.header}>
-          {/* < Navbar/> */}
-          {/* {home ? (
-            <>
-              <Image
-                priority
-                src="https://hgiven.github.io/portfolio/public/images/profile.jpg"
-                className={utilStyles.borderCircle}
-                height={144}
-                width={144}
-                alt=""
-              />
-              <h1 className={utilStyles.heading2Xl}>{name}</h1>
-            </>
-          ) : (
-            <>
-              <Link href="/">
-                <Image
-                  priority
-                  src="https://hgiven.github.io/portfolio/public/images/profile.jpg"
-                  className={utilStyles.borderCircle}
-                  height={108}
-                  width={108}
-                  alt=""
-                />
-              </Link>
-              <h2 className={utilStyles.headingLg}>
-                <Link href="/" className={utilStyles.colorInherit}>
-                  {name}
-                </Link>
-              </h2>
-            </>
-          )} */}
-        </header>
         <main>{children}</main>
         {!home && (
           <div className={styles.backToHome}>
