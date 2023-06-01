3e24bdb7f940ad088b6fc54725a5351ff1cfa153
Harrison Given <harrisonjbg@gmail.com>
2023-06-01 12:51:23 -0700

    made some changes

--- pages/posts/contact.js
+++ pages/posts/contact.js
@@ -1,3 +1,12 @@
 export default function Contact() {
-    return <h1>Contact</h1>;
-  }
+    return (
+      <>
+        <div style={{
+          backgroundImage: `url("https://hgiven.github.io/portfolio/images/bg.png")`,
+          height: '100vh',
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: 'cover',
+        }}/>
+      </>
+  )
+}
