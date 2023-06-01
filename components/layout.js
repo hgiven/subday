4c22197d07e31541cf64d01103ad719f9f75f357
Harrison Given <harrisonjbg@gmail.com>
2023-06-01 09:47:32 -0700

    Update layout.js

--- components/layout.js
+++ components/layout.js
@@ -15,7 +15,7 @@ export default function Layout({ children, home }) {
         <main>{children}</main>
         {!home && (
           <div className={styles.backToHome}>
-            <Link href="/">← Back to home</Link>
+            <Link href="/">← Previous page</Link>
           </div>
         )}
       </div>
