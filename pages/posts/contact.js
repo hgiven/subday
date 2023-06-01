0789f210c2300300786948bddd20aa23e3fe4d15
Harrison Given <harrisonjbg@gmail.com>
2023-06-01 13:09:40 -0700

    Update contact.js

--- pages/posts/contact.js
+++ pages/posts/contact.js
@@ -1,14 +1,18 @@
 export default function Contact() {
-    return (
-      <>
-        <div style={{
-          backgroundImage: `url("https://hgiven.github.io/portfolio/images/bg.png")`,
-          height: '100vh',
-          backgroundRepeat: 'no-repeat',
-          backgroundSize: 'cover',
-          zIndex: -1,
-        }}/>
-        <h1>Contact</h1>
-      </>
-  )
+  return <h1>Contact</h1>;
 }
+
+// export default function Contact() {
+//     return (
+//       <>
+//         <div style={{
+//           backgroundImage: `url("https://hgiven.github.io/portfolio/images/bg.png")`,
+//           height: '100vh',
+//           backgroundRepeat: 'no-repeat',
+//           backgroundSize: 'cover',
+//           zIndex: -1,
+//         }}/>
+//         <h1>Contact</h1>
+//       </>
+//   )
+// }
