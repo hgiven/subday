2db0bcf399aff28305715f298ae7e2ee860f4d5b
Harrison Given <harrisonjbg@gmail.com>
2023-06-01 14:12:53 -0700

    Update contact.js

--- pages/contact.js
+++ pages/contact.js
@@ -10,25 +10,25 @@ export default function Contact() {
 
       <section>
         <div className={utilStyles.grid}>
-          <>
+          <a>
             <h1>Hello</h1>
             <p>Take a look</p>
           </a>
 
-          <>
+          <a>
             <h1>Hello</h1>
             <p>Take a look</p>
           </a>
 
-          <>
+          <a>
             <h1>Hello</h1>
             <p>Take a look</p>
           </a>
 
-          <>
+          <a>
             <h1>Hello</h1>
             <p>Take a look</p>
-          </>
+          </a>
         </div>
       </section>
     </div>
